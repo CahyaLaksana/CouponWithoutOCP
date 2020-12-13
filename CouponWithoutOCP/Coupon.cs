884396dbf58bb10fd6f8e5128efb679e4171407a
CahyaLaksana <cahya.laksana@students.amikom.ac.id>
2020-12-13 23:32:12 +0700

    Add project files.

---
+++ CouponWithoutOCP/Coupon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithoutOCP
+{
+    class Coupon
+    {
+        public double discNominal = 0;
+        public double discPercentage = 0;
+        public double discashback = 0;
+        public double priceNett(double originPrice)
+        {
+            double net = 0;
+            double disc = 0;
+            if (discNominal == 0 && discPercentage > 0)
+            {
+                net = (100 - discPercentage) * originPrice / 100;
+            }
+            else if(discNominal > 0 && discPercentage == 0)
+            {
+                net = originPrice - discNominal;
+            }
+            else if(discNominal > 0 && discPercentage > 0)
+            {
+                disc = discPercentage *originPrice / 100;
+                if (disc > 3000)
+                {
+                    net = originPrice - 3000; 
+                }
+                else
+                {
+                    net = originPrice - disc;
+                }
+            }
+            return net;
+        }
+    }
+}
